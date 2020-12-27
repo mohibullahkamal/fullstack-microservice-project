4836ef95632ae0e348b563a1af3acb73e2244246
mogit2020 <mohibullah.kamal@gmail.com>
2020-12-27 14:16:01 +0100

    installing dependencies

---
+++ Blog/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+
+
+app.get('/', (req, res) => {
+  res.send('Hello Mohib...')
+})
+
+
+app.listen(5000);
