4e23e833ced799f06bbc6234982bd4bd1bfba8fd
mogit2020 <mohibullah.kamal@gmail.com>
2020-12-27 23:01:13 +0100

    add view for new data

--- Blog/routes/articles.js
+++ Blog/routes/articles.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send('in --> ../articles/...')
+router.get('/new', (req, res) => {
+  res.render('articles/new')
 });
 
 module.exports = router;
