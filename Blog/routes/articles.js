edbd4229ac46251d7578c77216884d17f8062d2a
mogit2020 <mohibullah.kamal@gmail.com>
2020-12-27 20:16:52 +0100

    built test data

--- Blog/routes/articles.js
+++ Blog/routes/articles.js
@@ -2,3 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 
+router.get('/', (req, res) => {
+  res.send('in --> ../articles/...')
+});
+
+
+module.exports = router;
--- Blog/server.js
+++ Blog/server.js
@@ -1,14 +1,23 @@
 const express = require('express');
+const articleRouter = require('./routes/articles')
 const app = express();
 
 app.set('view engine', 'ejs');
+
+app.use('/articles', articleRouter);
+
 
 // app.get('/', (req, res) => {
 //   res.send('Hello Mohib...')
 // })
 
 app.get('/', (req, res) => {
-  res.render('index')
+  // res.render('index', { 
+  //   text: 'Hello... this is "text" variable...'
+  // })
+  res.render('index', { 
+    getAllArticles: articleRouter
+  })
 })
 
 
