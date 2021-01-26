f88452659b173e890dca83c88a3eb31a851d4aa2
mogit2020 <mohibullah.kamal@gmail.com>
2021-01-26 09:07:32 +0100

    add index.js

---
+++ mongo-backend/src/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require("cors");
+
+const PORT = 3000;
+var app = express();
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
