7e4d1ef5de51f64e9be871c65329d1156a4efb01
mogit2020 <mohibullah.kamal@gmail.com>
2021-01-30 17:52:56 +0100

    edit itemModel

---
+++ mongo-backend/src/models/itemModel.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+var itemModel = mongoose.model('item', {
+   itemId:   { type: String },
+   itemName: { type: String }
+});
+
+module.exports = { itemModel };
